d1a09152d12a9c638843160b710f22c1ac65d6f5
adrian <tada@specyal.com>
2018-04-16 23:47:49 -0400

    some more files not my code

--- ProtocolTree/src/ui/PropertiesCellRenderer.java
+++ ProtocolTree/src/ui/PropertiesCellRenderer.java
@@ -23,10 +23,10 @@ public class PropertiesCellRenderer implements TreeCellRenderer {
 
 	DefaultTreeCellRenderer defaultRenderer = new DefaultTreeCellRenderer();
 	Color backgroundSelectionColor;
-	Color backgroundNonSelectionColor;
+	Color backgroundNonSelectionColor;//
 	//ImageIcon createImageIcon = ResourceFindingDummyClass.createImageIcon("pipette.png");
 	Icon iconObjectProperty = UIManager.getIcon("FileChooser.detailsViewIcon");// http://en-human-begin.blogspot.ca/2007/11/javas-icons-by-default.html
-	Icon iconDataProperty = UIManager.getIcon("Tree.leafIcon");// http://en-human-begin.blogspot.ca/2007/11/javas-icons-by-default.html
+	Icon iconDataProperty = UIManager.getIcon("Tree.leafIcon");     // http://en-human-begin.blogspot.ca/2007/11/javas-icons-by-default.html
 
 	public PropertiesCellRenderer() {
 		backgroundSelectionColor = defaultRenderer.getBackgroundSelectionColor();
---
+++ game/desktop/src/protocol/game/desktop/old/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package protocol.game.desktop.old;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import protocol.game.ProtocolGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProtocolGame(), config);
+	}
+}
