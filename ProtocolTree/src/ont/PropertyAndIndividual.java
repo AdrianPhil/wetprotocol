cca1254f0fbce49b1808335a71cddff13614475b
adrian <tada@specyal.com>
2018-04-25 23:31:55 -0400

    almost works need testing

--- ProtocolTree/src/ont/PropertyAndIndividual.java
+++ ProtocolTree/src/ont/PropertyAndIndividual.java
@@ -14,7 +14,7 @@ public class PropertyAndIndividual {
 	boolean fromRangeSuperclasses = false;
 	NodeType nodeType;
 
-	public PropertyAndIndividual(OntProperty ontProperty,  Individual individual, NodeType nodeType) {
+	public PropertyAndIndividual(OntProperty ontProperty, Individual individual, NodeType nodeType) {
 		this.ontProperty = ontProperty;
 		this.individual = individual;
 		this.nodeType = nodeType;
@@ -49,16 +49,19 @@ public class PropertyAndIndividual {
 	public NodeType getNodeType() {
 		return nodeType;
 	}
-
-//	public DisplayType getDisplayType() {
-//		if (fromRangeSuperclasses)
-//			return DisplayType.FROM_RANGE_SUPERCLASS_PROPERTY_OBJECT;
-//		if (ontProperty.isLiteral())
-//			return DisplayType.LITERAL;
-//		return DisplayType.IN_CLASS_PROPERTY_OBJECT;
-//	}
 //
-//	public enum DisplayType {
-//		LITERAL, FROM_RANGE_SUPERCLASS_PROPERTY_OBJECT, IN_CLASS_PROPERTY_OBJECT
+//	public void setIndividual(Individual createIndividual) {
+//		this.individual = individual;
 //	}
+	// public DisplayType getDisplayType() {
+	// if (fromRangeSuperclasses)
+	// return DisplayType.FROM_RANGE_SUPERCLASS_PROPERTY_OBJECT;
+	// if (ontProperty.isLiteral())
+	// return DisplayType.LITERAL;
+	// return DisplayType.IN_CLASS_PROPERTY_OBJECT;
+	// }
+	//
+	// public enum DisplayType {
+	// LITERAL, FROM_RANGE_SUPERCLASS_PROPERTY_OBJECT, IN_CLASS_PROPERTY_OBJECT
+	// }
 }
